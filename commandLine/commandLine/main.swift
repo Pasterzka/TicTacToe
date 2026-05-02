5a902ba971acdf6f8cf37a5e14b56b8963b1c714
Pasterzka <jaku6p@gmail.com>
2026-05-02 21:39:43 +0200

    feat: Added game score

--- commandLine/commandLine/main.swift
+++ commandLine/commandLine/main.swift
@@ -7,16 +7,27 @@
 
 import Foundation
 
-
+// game configuration
 var board: [String] = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 var isGameOn: Bool = true
 var currentPlayer: String = "X"
-var correctMove: Bool = false
+var isPvE: Bool = false
+var startPlayer: String = "X"
 
+// score
+var scoreX = 0
+var scoreO = 0
+var scoreDraws = 0
 
-// print board 3x3
-func printBoard() -> Void{
+// print game
+func printGame() -> Void{
+    print("\n")
     print("Now Playing: \(currentPlayer)")
+    printBoard()
+}
+
+// print board
+func printBoard() -> Void{
     print("")
     
     print("  0|  1|  2")
@@ -35,6 +46,9 @@ func printBoard() -> Void{
 func makeMove(player: String) -> Void {
     print("Enter the number of the position you want to place your \(currentPlayer)")
     
+    
+    
+    var correctMove: Bool = false
     // move logic
     while !correctMove{
         if let input = readLine(), let position = Int(input), position >= 0, position <= 8 {
@@ -81,22 +95,62 @@ func checkWinner(player: String) -> Bool {
     return false
 }
 
+// check if there is an empty place
+func isBoardFull() -> Bool{
+    return !board.contains(" ")
+}
 
 // program
 while isGameOn {
+    // print board
     printBoard()
-    makeMove(player: currentPlayer)
-    isGameOn = !checkWinner(player: currentPlayer)
     
-    if !isGameOn{
-        print("Player \(currentPlayer) won!")
-        print("Game Ended")
+    // player or computer move
+    if isPvE && currentPlayer == "O"{
+        print("NOT IMPLEMENTED")
+    }
+    else {
+        makeMove(player: currentPlayer)
     }
     
-    if currentPlayer == "X"{
-        currentPlayer = "O"
-    }else{
-        currentPlayer = "X"
+    // check win or draw -> next player
+    if checkWinner(player: currentPlayer){
+        print("Player \(currentPlayer) win the GAME!")
+        
+        if currentPlayer == "X" {
+            scoreX += 1
+        }
+        else {
+            scoreO += 1
+        }
+        
+        isGameOn = false
+    }
+    else if isBoardFull(){
+        scoreDraws += 1
+        isGameOn = false
+        print("DRAW!")
+    }
+    else {
+        currentPlayer = (currentPlayer == "X") ? "O" : "X"
+    }
+    
+    
+    if !isGameOn{
+        printBoard()
+        print()
+        print(" --- SCORE --- ")
+        print("X: \(scoreX), O: \(scoreO), DRAW: \(scoreDraws)")
+        
+        print("Wonna play next? (t/n)")
+        if readLine() == "t"{
+            board = Array(repeating: " ", count: 9)
+            isGameOn = true
+            startPlayer = (startPlayer == "X") ? "O" : "X"
+            currentPlayer = startPlayer
+        }else{
+            print("Thank you for a game! BYE!")
+        }
     }
 }
 
